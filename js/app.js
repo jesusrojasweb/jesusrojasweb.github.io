530c6c0759e774b0e33f002f38021f815420418d
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2021-01-04 16:41:14 -0400

    El portafolio ahora tiene todo los proyectos en vez de un solo tipo. Menu del proyecto

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+let portafolioButtons = document.querySelectorAll('.portafolio-menu-item a');
+
+for(const boton of portafolioButtons){
+    boton.addEventListener('click',(e)=>{
+        e.preventDefault();
+    })
+}
+
 console.log()
